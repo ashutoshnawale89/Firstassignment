176c2a0025a6ae07c0957b639d8865eb43e009a2
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-06-30 13:42:47 +0530

    addingwageformonth

--- wage.sh
+++ wage.sh
@@ -1,20 +1,22 @@
 #!/bin/bash -x
 
-isemployee=1;
-
+isfulltimeemployee=1;
+isparttimeemployee=2;
 employeerateperhour=20;
+workingdays=20;
 
-
-empcheck=$((RANDOM%2)); 
+for (( day=1; day<=$workingdays; day++ ))
+do
+empcheck=$((RANDOM%2))
 
 case $empcheck in
-$isemployee)
-    emphrsperday=8;;
-*)
-emphrsperday=0;;
+$isfulltimeemployee)
+                   emphrsperday=8;;
+$isparttimeemployee)
+                 emphrsperday=8;;
 
 esac
 sum=$(($employeerateperhour * $emphrsperday ));
 
-echo " employee wages is $sum "
-
+echo " employee has earned $sum per month "
+ done
