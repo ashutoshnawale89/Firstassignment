7961d0f138dcf15a12eee962d4bdc3ca32750f81
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-06-30 14:14:39 +0530

    caclculatewagesfor100hours

---
+++ wage2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+isfulltimepresent=1;
+isworkingdayspermonth=20;
+empratehr=20;
+
+case $empcheck in
+$fulltimepresent)
+                  emphrsinmonth=100;;
+
+esac
+salary=$(($emphrsinmonth * $empratehr));
+echo " salary of employee is $salary in the month"
+
+
